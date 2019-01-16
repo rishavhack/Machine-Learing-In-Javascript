7817b901182f5cff2e44e7c575ef119a1b54c050
Rishav <rishav.jnit@gmail.com>
2019-01-16 20:39:06 +0530

    droppostion,bounciness,size,bucketball

--- MLKits-master/plinko/score.js
+++ MLKits-master/plinko/score.js
@@ -1,5 +1,5 @@
 const outputs = [];
-
+//This is getting all ball detail
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   outputs.push([dropPosition,bounciness,size,bucketLabel])
   console.log(outputs);
